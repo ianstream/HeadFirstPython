d74d8e3b15e3c2ff7f8234172f14edebf7a9e306
ianstream <ianstream@naver.com>
2016-01-31 20:13:43 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author= 'ianstream',
+    author_email = 'ianstream@gmail.com',
+    url = '',
+    description = 'A simple printer of nested lists',
+)
