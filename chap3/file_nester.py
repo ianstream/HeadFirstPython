0f061edd6662ac53b5a902deb950a4f2f1c720fb
ianstream <ianstream@naver.com>
2016-02-07 22:59:18 +0900

    chg file_nester, add test

--- chap3/file_nester.py
+++ chap3/file_nester.py
@@ -1,14 +1,18 @@
-import os
 
-data = open('txt/sketch.txt')
+data = None
 
-for each_line in data:
-    if not each_line.find(':') == -1:
-        (role, line_spoken) = each_line.split(':', 1)
-        print(role, end='')
-        print(' said: ', end='')
-        print(line_spoken, end='')
-    else:
-        print(each_line)
+def file_open():
+    data = open('txt/sketch.txt')
 
-data.close()
+def print_file():
+    for each_line in data:
+        if not each_line.find(':') == -1:
+            (role, line_spoken) = each_line.split(':', 1)
+            print(role, end='')
+            print(' said: ', end='')
+            print(line_spoken, end='')
+        else:
+            print(each_line)
+
+def file_close():
+    data.close()
---
+++ chap3/test/test.py
@@ -0,0 +1,6 @@
+import file_nester
+
+file_nester.file_open()
+file_nester.print_file()
+file_nester.file_close()
+
