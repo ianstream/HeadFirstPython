a2f511266b9337768033b4758316fcf560f410ce
ianstream <ianstream@naver.com>
2016-02-08 00:12:35 +0900

    add tow list for man, other's spoken

--- chap3/setup.py
+++ chap3/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'file_nester',
-    version = '1.0.0',
+    version = '1.1.0',
     py_modules = ['file_nester'],
     author= 'ianstream',
     author_email = 'ianstream@gmail.com',
