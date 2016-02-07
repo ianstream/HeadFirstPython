41c0bb6b05c8bdc1262ba3c32fdfa5385de4191e
ianstream <ianstream@naver.com>
2016-02-08 00:14:21 +0900

    delete txt dir, add chap4

---
+++ chap4/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'file_nester',
+    version = '1.1.0',
+    py_modules = ['file_nester'],
+    author= 'ianstream',
+    author_email = 'ianstream@gmail.com',
+    url = '',
+    description = '',
+)
