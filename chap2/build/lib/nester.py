170370a959af087310960023b709719a99de6027
ianstream <ianstream@naver.com>
2016-02-08 16:53:46 +0900

    write two files

--- chap2/build/lib/nester.py
+++ chap2/build/lib/nester.py
@@ -2,14 +2,16 @@
 입력받은 리스트의 각 원소를 출력한다
 원소가 리스트인 경우엔 재귀호출을 통해 indent 를 적용 후 원소를 출력한다
 """
-def print_lol(the_list, indent=False, level=0):
+import sys
+
+def print_lol(the_list, indent=False, level=0, fh=sys.stdout):
     if level < 0:
         level = 0
     for each_item in the_list:
         if isinstance(each_item, list):
-            print_lol(each_item, indent, level+1)
+            print_lol(each_item, indent, level+1, fh)
         else:
             if indent:
                 for tab_stab in range(level):
-                    print("\t", end='')
-            print(each_item)
+                    print("\t", end='', file=fh)
+            print(each_item, file=fh)
--- chap4/build/lib/write_file.py
+++ chap4/build/lib/write_file.py
@@ -1,3 +1,5 @@
+import nester
+
 def write_files():
     print("*** print_file ***")
 
@@ -10,25 +12,24 @@ def write_files():
     other = []
 
     try:
-        data = open('sketch.txt')
+        with open('sketch.txt') as data:
+            for each_line in data:
+                try:
+                    (role, line_spoken) = each_line.split(':', 1)
+                    line_spoken = line_spoken.strip()
 
-        for each_line in data:
-            try:
-                (role, line_spoken) = each_line.split(':', 1)
-                line_spoken = line_spoken.strip()
+                    if role == 'Man':
+                        man.append(line_spoken)
+                    elif role == 'Other Man':
+                        other.append(line_spoken)
 
-                if role == 'Man':
-                    man.append(line_spoken)
-                elif role == 'Other Man':
-                    other.append(line_spoken)
+                    print(role, end='')
+                    print(' said: ', end='')
+                    print(line_spoken, end='')
+                except ValueError:
+                    pass
 
-                print(role, end='')
-                print(' said: ', end='')
-                print(line_spoken, end='')
-            except ValueError:
-                pass
-
-        data.close()
+            data.close()
 
     except IOError:
         print("The data file is mission")
@@ -41,18 +42,13 @@ def write_files():
     """
     open files to write man, other's list
     write lists
-    close files
+    close files using 'with'
     """
     try:
-        man_file = open('man_data.txt', 'w')
-        other_file = open('other_data.txt', 'w')
-
-        print(man, file=man_file)
-        print(other, file=other_file)
+        with open('man_data.txt', 'w') as man_file, \
+             open('other_data.txt', 'w') as other_file:
+            nester.print_lol(man, True, 0, fh=man_file)
+            nester.print_lol(other, True, 0, fh=other_file)
 
     except IOError:
         print('File Error !!!')
-
-    finally:
-        man_file.close()
-        other_file.close()
