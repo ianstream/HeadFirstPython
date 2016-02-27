7b84841141ffa3fe8fc805c19c7a94b1591c2ea2
ianstream <ianstream@naver.com>
2016-02-27 21:11:40 +0900

    add chap6

--- chap6/coach.py
+++ chap6/coach.py
@@ -1,41 +1,1 @@
-
-def print_list():
-
-    try:
-        james_list = get_coach_data('james2.txt')
-        julie_list = get_coach_data('julie2.txt')
-        mikey_list = get_coach_data('mikey2.txt')
-        sarah_list = get_coach_data('sarah2.txt')
-
-        print('james list : ', sorted(set([sanitize(each_list) for each_list in james_list]))[0:3])
-        print('julie list : ', sorted(set([sanitize(each_list) for each_list in julie_list]))[0:3])
-        print('mikey list : ', sorted(set([sanitize(each_list) for each_list in mikey_list]))[0:3])
-        print('sarah list : ', sorted(set([sanitize(each_list) for each_list in sarah_list]))[0:3])
-
-    except IOError:
-        print("The data file is mission")
-
-def get_coach_data(filename):
-    try:
-        with open(filename) as datas:
-            data = datas.readline()
-        return data.strip().split(',')
-    except IOError as ioerr:
-        print('File Error!!! ' + str(ioerr))
-        return None
-
-def sanitize(time_string):
-    """
-    get a string and retrun formated string(ex 5.43)
-    :param time_string: string
-    :return: formatted string
-    """
-    if '-' in time_string:
-        splitter = '-'
-    elif ':' in time_string:
-        splitter = ':'
-    else:
-        return time_string
-
-    (mins, secs) = time_string.split(splitter)
-    return mins + '.' + secs
+class AthleteList(list):    def __init__(self, a_name, a_dob=None, a_times=[]):        list.__init__([])        self.name = a_name        self.dob = a_dob        self.extend(a_times)    def top3(self):        return sorted(set([sanitize(t) for t in self]))[0:3]def print_dict():    try:        james = get_coach_data('james2.txt')        julie = get_coach_data('julie2.txt')        mikey = get_coach_data('mikey2.txt')        sarah = get_coach_data('sarah2.txt')        print(james.name + "'s fastest times are: " + str(james.top3()))        print(julie.name + "'s fastest times are: " + str(julie.top3()))        print(mikey.name + "'s fastest times are: " + str(mikey.top3()))        print(sarah.name + "'s fastest times are: " + str(sarah.top3()))    except IOError:        print("The data file is mission")def get_coach_data(filename):    try:        with open(filename) as datas:            data = datas.readline()        temp_list = data.strip().split(',')        return AthleteList(temp_list.pop(0), temp_list.pop(0), temp_list)    except IOError as ioerr:        print('File Error!!! ' + str(ioerr))        return Nonedef sanitize(time_string):    """    get a string and retrun formated string(ex 5.43)    :param time_string: string    :return: formatted string    """    if '-' in time_string:        splitter = '-'    elif ':' in time_string:        splitter = ':'    else:        return time_string    (mins, secs) = time_string.split(splitter)    return mins + '.' + secs
